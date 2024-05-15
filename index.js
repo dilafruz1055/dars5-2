c15f87e0abd5078fbd3732a6c38e0c9adb24ca47
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-05-15 11:44:17 +0500

    dan

---
+++ index.js
@@ -0,0 +1,10 @@
+let ism = prompt("Ismingizni kiriting");
+let id = prompt("Id kiriting");
+let passport = prompt("passportingizni kiriting");
+ let data = {
+    ism: ism,
+    id: id,
+    passport: passport,
+ }
+ 
+ console.log(data)
